22906f573eb1410d162f11d3de0fa0d774dec734
Wonmi <wonmadesign@gmail.com>
2018-11-22 21:20:57 +0900

    HTTP request added

--- app.js
+++ app.js
@@ -1,21 +1,79 @@
 const puzzleEl = document.querySelector('#puzzle')
 const guessNumEl = document.querySelector('#guess-num')
-const firstQuiz = new Hangman('cat', 3)
+const firstQuiz = new Hangman('cat toy', 3)
 const secondQuiz = new Hangman('cocacola', 3)
 
 
 // Display puzzle and guessNum
-const showResult = function (firstQuiz) {
-    puzzleEl.textContent = firstQuiz.getPuzzle()
-    guessNumEl.textContent = firstQuiz.showMessage()
-}
+
+puzzleEl.textContent = firstQuiz.puzzle
+guessNumEl.textContent = firstQuiz.message
 
 // keypress event
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const letterInput = String.fromCharCode(e.charCode)
     firstQuiz.getLetter(letterInput) // push to array + decrement if needed
-    showResult(firstQuiz)
+    // 이녀석을 firstQuiz.letter = letterInput 으로해서 setter 설정할 수 있긴하지만, 
+    // firstQuiz.letter값에 접근할 일이 없으면 굳이 setter로 코딩할 이유가 없음
+    // method는 지금처럼 method스럽게 남겨둘 필요도 있음. (value를 fetch할 게 아니라면)
+
+    puzzleEl.textContent = firstQuiz.puzzle
+    guessNumEl.textContent = firstQuiz.message
 })
 
-// Default display
-showResult(firstQuiz)
+const request = new XMLHttpRequest()
+
+// 어떤 상황이 감지되었을 때 해당 response(string)을 JSON parse하여 object로 변환하고싶어
+request.addEventListener('readystatechange', (e) => {
+    // 두 조건이 성립 할 때
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const quizText = JSON.parse(e.target.responseText)
+        console.log(quizText)
+    } else if (e.target.readyState === 4) {
+        console.log('An error has taken place.')
+    }
+    // 주소에 오류가 있으면 400 status 로 뜸
+})
+
+request.open("GET", "http://puzzle.mead.io/puzzle") // initialize a request
+request.send() // sends the request to the server. 
+// If the request is asynchronous (which is the default), 
+// this method returns as soon as the request is sent and 
+// the result is delivered using events.
+// 이까지하면 바로 response에 데이터 들어옴.
+
+
+
+// Mission
+// 1. Make a request for all countries!     "http://restcountries.eu/"
+// 2. Parse 'responseText' to get back array of objects.
+// 3. Find your country object by its country code (prop name : alpha2Code)
+// 4. Print the full country name (prop name : name)
+
+
+const request2 = new XMLHttpRequest()
+
+request2.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const countries = JSON.parse(e.target.responseText)
+        const country = countries.find((element) => element.alpha2Code === "US")
+        console.log(country.name)
+
+    } else if (e.target.readyState === 4) {
+        console.log('Error has been detected.')
+    }
+})
+
+request2.open("GET", "https://restcountries.eu/rest/v2/all")
+request2.send()
+
+
+
+
+// const countryCode = "KR"
+
+
+
+
+
+
---
+++ hangman_class.js
@@ -0,0 +1,69 @@
+class Hangman {
+
+    constructor(word, guessNum) {
+        this.word = word.toLowerCase().split('')
+        this.guessNum = guessNum,
+        this.guessedLetters = [],
+        this.status = 'playing'
+    }
+
+    checkStatus () {
+        const remainingGuesses = this.guessNum > 0
+        const allMatched = this.puzzle === this.word.join('')
+
+        if (remainingGuesses && allMatched) {
+            this.status = 'success'
+        } else if (remainingGuesses && !allMatched) { // 숫자 0이하되고 매치안되었을경우
+            this.status = 'playing'
+        } else {
+            this.status = 'fail'  // 숫자안남을 경우들
+        }
+
+        console.log(this.status)
+    }
+
+    getLetter (letterInput) {   //getLetter = "값" 이렇게 받아들이고 처리하는 상황인가?
+        if (this.status !== 'playing') {
+            return
+        }
+
+        const isNewLetter = !this.guessedLetters.includes(letterInput)
+        const isNoMatch = !this.word.includes(letterInput)
+
+        if (isNewLetter) {
+            this.guessedLetters.push(letterInput)
+        } else {
+            alert('You already typed ' + letterInput)
+        }
+
+        if (isNewLetter && isNoMatch) {
+            this.guessNum--
+        }
+        this.checkStatus()
+    }
+
+    get puzzle () {   // 보이진 않지만 this.puzzle 을 만들게 됨 
+        let output = ''
+
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
+                return output = output + letter
+            } else {
+                return output = output + '*'
+            }
+        })
+
+        return output
+    }
+    
+    get message () {
+        if (this.status === 'playing') {
+            return `Guesses left : ${firstQuiz.guessNum}`
+        } else if (this.status === 'success') {
+            return 'Greate work! You guessed the word.'
+        } else {
+            return `Nice try! The word was "${this.word.join('')}"`
+        }
+    }
+
+}
